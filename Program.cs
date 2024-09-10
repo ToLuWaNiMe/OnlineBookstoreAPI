98e753a01138d032e0dbe0995bcdcb71d0d68631
Bahdtoluwani <oladosu.toluene@gmail.com>
2024-09-10 19:41:23 +0100

    little modification

--- Program.cs
+++ Program.cs
@@ -18,13 +18,11 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<TokenService>();
 
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
 var jwtSettings = builder.Configuration.GetSection("Jwt");
-
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
